7689a7915569dd5e0711de077f845c1625b51f08
ivm <stan777.durand777@gmail.com>
2024-08-22 14:12:57 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -63,8 +63,15 @@ setup(
         CUDAExtension('cuda_ba',
             sources=['dpvo/fastba/ba.cpp', 'dpvo/fastba/ba_cuda.cu'],
             extra_compile_args={
-                'cxx':  ['-O3'], 
-                'nvcc': ['-O3'],
+                'cxx': ['-O3'],
+                'nvcc': ['-O3',
+                    '-gencode=arch=compute_60,code=sm_60',
+                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_70,code=sm_70',
+                    '-gencode=arch=compute_75,code=sm_75',
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',
+                ]
             }),
         CUDAExtension('lietorch_backends', 
             include_dirs=[
@@ -74,7 +81,18 @@ setup(
                 'dpvo/lietorch/src/lietorch.cpp', 
                 'dpvo/lietorch/src/lietorch_gpu.cu',
                 'dpvo/lietorch/src/lietorch_cpu.cpp'],
-            extra_compile_args={'cxx': ['-O3'], 'nvcc': ['-O3'],}),
+            extra_compile_args={
+                'cxx': ['-O3'],
+                'nvcc': ['-O3',
+                    '-gencode=arch=compute_60,code=sm_60',
+                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_70,code=sm_70',
+                    '-gencode=arch=compute_75,code=sm_75',
+                    '-gencode=arch=compute_80,code=sm_80',
+                    '-gencode=arch=compute_86,code=sm_86',
+                ]
+            }),
+
     ],
     cmdclass={
         'build_ext': BuildExtension
