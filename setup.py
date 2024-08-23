1354592730b8f0894a24259c9446e37db06bfee5
ivm <stan777.durand777@gmail.com>
2024-08-23 10:08:51 +0200

    update encore cmake pour test

--- setup.py
+++ setup.py
@@ -12,37 +12,6 @@ import os.path as osp
 ROOT = osp.dirname(osp.abspath(__file__))
 
 
-# coucou
-
-
-# setup(
-#     name='droid_backends',
-#     ext_modules=[
-#         CUDAExtension('droid_backends',
-#             include_dirs=[osp.join(ROOT, 'thirdparty/eigen-3.4.0')],
-#             # sources=[
-#             #     'src/droid.cpp', 
-#             #     'src/droid_kernels.cu',
-#             #     'src/correlation_kernels.cu',
-#             #     'src/altcorr_kernel.cu',
-#             # ],
-#             sources=['dpvo/altcorr/correlation.cpp', 'dpvo/altcorr/correlation_kernel.cu'],
-#             extra_compile_args={
-#                 'cxx': ['-O3'],
-#                 'nvcc': ['-O3',
-#                     '-gencode=arch=compute_60,code=sm_60',
-#                     '-gencode=arch=compute_61,code=sm_61',
-#                     '-gencode=arch=compute_70,code=sm_70',
-#                     '-gencode=arch=compute_75,code=sm_75',
-#                     '-gencode=arch=compute_80,code=sm_80',
-#                     '-gencode=arch=compute_86,code=sm_86',
-#                 ]
-#             }),
-#     ],
-#     cmdclass={ 'build_ext' : BuildExtension }
-# )
-
-
 
 setup(
     name='dpvo',
