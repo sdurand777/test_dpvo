11a6099ecd96b16c0b86056a0ccc584b1d002e3d
ivm <stan777.durand777@gmail.com>
2024-09-04 09:56:55 +0200

    update ajout branch spu et modif sm arch

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
             extra_compile_args={
                 'cxx': ['-O3'],
                 'nvcc': ['-O3',
-                    '-gencode=arch=compute_60,code=sm_60',
-                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_86,code=sm_86',
+                    #'-gencode=arch=compute_61,code=sm_61',
                     # '-gencode=arch=compute_70,code=sm_70',
                     # '-gencode=arch=compute_75,code=sm_75',
                     # '-gencode=arch=compute_80,code=sm_80',
@@ -34,8 +34,8 @@ setup(
             extra_compile_args={
                 'cxx': ['-O3'],
                 'nvcc': ['-O3',
-                    '-gencode=arch=compute_60,code=sm_60',
-                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_86,code=sm_86',
+                    #'-gencode=arch=compute_61,code=sm_61',
                     # '-gencode=arch=compute_70,code=sm_70',
                     # '-gencode=arch=compute_75,code=sm_75',
                     # '-gencode=arch=compute_80,code=sm_80',
@@ -53,8 +53,8 @@ setup(
             extra_compile_args={
                 'cxx': ['-O3'],
                 'nvcc': ['-O3',
-                    '-gencode=arch=compute_60,code=sm_60',
-                    '-gencode=arch=compute_61,code=sm_61',
+                    '-gencode=arch=compute_86,code=sm_86',
+                    #'-gencode=arch=compute_61,code=sm_61',
                     # '-gencode=arch=compute_70,code=sm_70',
                     # '-gencode=arch=compute_75,code=sm_75',
                     # '-gencode=arch=compute_80,code=sm_80',
